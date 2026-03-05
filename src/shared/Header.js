7486004a52a26b4fe33f09be2c2a32a30a1a1030
Tsukadaa16 <tsukadamarcelo4@gmail.com>
2026-03-04 22:40:26 -0300

    Aula 02 - Login e Home pages

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -1,8 +1,9 @@
 export function createHeader(pageName) {
     return `
         <ion-header>
-            <ion-toolbar color="primary">
-                <ion-title>${pageName}</ion-title>
+            <ion-toolbar color="secondary">
+                <ion-icon name="cafe" style="margin-left: 15px; font-size: 24px" slot="start"></ion-icon>
+                <ion-tittle>Quero Café Bar - ${pageName}</ion-tittle>
             </ion-toolbar>
         </ion-header>
     `;
